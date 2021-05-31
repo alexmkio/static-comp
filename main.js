37cb2ccc00d7363421fb5fd44ae436cb21802312
Alex Kio <alexandermartelkio@gmail.com>
2021-05-31 14:31:39 -0400

    Add js to close hamburger menu on link click

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-var hamburger = document.querySelector('#hamburger');
+const hamburger = document.querySelector('#hamburger');
 hamburger.addEventListener('click', animateBurger);
+
+const nav = document.querySelector('nav');
+
+const navLink = document.querySelectorAll(".active");
+navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function animateBurger() {
   hamburger.classList.toggle("change");
+  nav.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("change");
+  nav.classList.remove("active");
 }
